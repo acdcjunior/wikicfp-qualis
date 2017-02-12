95102ad9843d0e080b1b09f98d49b0f85f354a70
Antonio Jr <acdcjunior@gmail.com>
2017-02-12 21:04:09 -0200

    dados: incluir coluna sigla_efetiva

--- crud/ddl.sql
+++ crud/ddl.sql
@@ -2,6 +2,7 @@ CREATE TABLE `qualis` (
   `id` int(10) NOT NULL auto_increment,
   `titulo` varchar(150) NOT NULL,
   `sigla` varchar(50) NULL,
+  `sigla_efetiva` varchar(50) NOT NULL,
   `qualis` char(2) NOT NULL,
   `issn` varchar(50) NULL,
   `area_avaliacao` varchar(50) NULL,
