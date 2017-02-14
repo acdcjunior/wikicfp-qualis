bb3edb9af3ca89ce8ac187c6d9bc645d9a0e4629
Antonio Jr <acdcjunior@gmail.com>
2017-02-14 12:18:12 -0200

    sigla: ajustar texto do link

--- calculo/sigla.php
+++ calculo/sigla.php
@@ -5,7 +5,7 @@ include('calcularSiglaSintetica.php');
 ?>
 
 <ul>
-    <li><a href='auto_sigla_efetiva.php'>Calcular sigla efetiva sintetica para todos com sigla "DUMMY"</a></li>
+    <li><a href='auto_sigla_efetiva.php'>Calcular sigla efetiva sintetica para todos de campo <code>sigla</code> com valor <code>NULL</code>.</a></li>
 </ul>
 
 
