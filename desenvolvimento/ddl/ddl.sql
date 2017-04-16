f5993d3d22bdc2d7b04b879b76e2e71c5070d915
Antonio Jr <acdcjunior@gmail.com>
2017-04-16 17:44:42 -0300

    DDL: ajustar collate e criar indice

--- desenvolvimento/ddl/ddl.sql
+++ desenvolvimento/ddl/ddl.sql
@@ -4,12 +4,15 @@ CREATE USER 'u910267182_user'@'%%' IDENTIFIED BY 'Y=w95aHm8j12pAsW:2';
 GRANT ALL ON u910267182_quali.* TO 'u910267182_user'@'%%';
 
 CREATE TABLE `u910267182_quali`.`qualis` (
-  `id` int(10) NOT NULL auto_increment,
-  `sigla` varchar(50) NULL,
-  `sigla_efetiva` varchar(50) NOT NULL,
-  `titulo` varchar(150) NOT NULL,
-  `qualis` char(2) NOT NULL,
-  `fonte` varchar(255) NOT NULL,
-  `metadados` varchar(20000) not null default '{}',
+  `id`            INT(10)        NOT NULL              AUTO_INCREMENT,
+  `sigla`         VARCHAR(50)    NULL                  CHARACTER SET UTF8_GENERAL_CI,
+  `sigla_efetiva` VARCHAR(50)    NOT NULL              CHARACTER SET UTF8_GENERAL_CI,
+  `titulo`        VARCHAR(150)   NOT NULL              CHARACTER SET UTF8_GENERAL_CI,
+  `qualis`        CHAR(2)        NOT NULL              CHARACTER SET UTF8_GENERAL_CI,
+  `fonte`         VARCHAR(255)   NOT NULL              CHARACTER SET UTF8_GENERAL_CI,
+  `metadados`     VARCHAR(20000) NOT NULL DEFAULT '{}' CHARACTER SET UTF8_GENERAL_CI,
   PRIMARY KEY  (`id`)
 );
+
+ALTER TABLE `qualis` ADD INDEX(`sigla`);
+ALTER TABLE `qualis` ADD INDEX(`sigla_efetiva`);
