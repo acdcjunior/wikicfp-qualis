f73a977095bc23248d812f45cf4a18372db4ce4b
Antonio Jr <acdcjunior@gmail.com>
2017-04-16 21:34:22 -0300

    chore: adicionar comentario importante para deploy

--- desenvolvimento/deploy.bat
+++ desenvolvimento/deploy.bat
@@ -1,3 +1,3 @@
-:: @echo off
+:: IMPORTANTE: para o deploy funcionar, voce tem que ter feito o push para o origin!!!
 set /p userpass="Usuario/Senha para deploy via Git-FTP? "
 docker run --rm -it mwienk/docker-git-ftp bash -c "git clone https://github.com/acdcjunior/wikicfp-qualis.git . && git ftp push -u u910267182.%userpass% -p %userpass% -P ftp://ftp.wikicfpqualis.esy.es"
